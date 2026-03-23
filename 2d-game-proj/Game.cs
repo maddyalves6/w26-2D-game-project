80e0bf6917f2e5c8613c6337926f09ded425af4a
maddyalves6 <madeline.alves@mohawkcollege.ca>
2026-03-23 19:25:00 -0400

    initial commit of vs project

---
+++ 2d-game-proj/Game.cs
@@ -0,0 +1,26 @@
+﻿// Include the namespaces (code libraries) you need below.
+using System;
+using System.Numerics;
+
+// The namespace your code is in.
+namespace MohawkGame2D
+{
+    
+    public class Game
+    {
+        //
+
+        // Setup runs once before the game loop begins.
+        public void Setup()
+        {
+
+        }
+
+        // Update runs every frame.
+        public void Update()
+        {
+
+        }
+    }
+
+}
