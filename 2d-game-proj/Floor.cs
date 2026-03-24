fb9f34463d81dab68088786567c89b3418e5e7ac
maddyalves6 <madeline.alves@mohawkcollege.ca>
2026-03-23 20:17:39 -0400

    program floor drawing

---
+++ 2d-game-proj/Floor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+// The namespace your code is in.
+namespace MohawkGame2D
+{
+	public class Floor
+	{
+		float relativeX = 0;
+
+		// draw the floor at the current relativeX position
+		public void DrawFloor()
+		{
+			for (int i = 0; i < 21; i++)
+			{
+				float originX = relativeX + (i * 40);
+
+				Draw.FillColor = Color.Black;
+				Draw.LineColor = new Color(230, 230, 230); // dark gray
+				Draw.LineSize = 2;
+
+				Draw.Rectangle(originX, 760, 40, 40);
+                Draw.Rectangle(originX, 720, 40, 40);
+
+				// draw floor details
+				Draw.LineSize = 0;
+				Draw.FillColor = Draw.LineColor;
+				Draw.Rectangle(originX + 28, 720, 2, 80); // dividing line
+				Draw.Rectangle(originX + 28, 770, 12, 2); // upper right lines
+                Draw.Rectangle(originX + 28, 730, 12, 2);
+				Draw.Rectangle(originX, 790, 28, 2); // lower left lines
+				Draw.Rectangle(originX, 750, 28, 2);
+            }
+		}
+
+		// Step the infinite floor's relativeX position "by" the given float, accounting for the looping, then DrawFloor()
+		public void Step(float by)
+		{
+			relativeX += by;
+			relativeX %= 40;
+			DrawFloor();
+		}
+	}
+}
--- 2d-game-proj/Game.cs
+++ 2d-game-proj/Game.cs
@@ -8,18 +8,20 @@ namespace MohawkGame2D
     
     public class Game
     {
-        //
+        Floor Floor = new Floor();
 
         // Setup runs once before the game loop begins.
         public void Setup()
         {
-
+            Window.SetSize(800, 800);
         }
 
         // Update runs every frame.
         public void Update()
         {
+            Window.ClearBackground(Color.OffWhite);
 
+            Floor.Step(-4);
         }
     }
 
